d4b7b4e773057d48a9e93183c585159986de6c64
Kun Qian <kunqian@live.ca>
2015-02-17 04:18:38 -0500

    fixed some code

--- switch_to_pm.asm
+++ switch_to_pm.asm
@@ -1,4 +1,4 @@
-[bits 16]
+[bits 16]    
 ; Switch to protected mode
 switch_to_pm:
     cli
@@ -23,26 +23,21 @@ init_pm:
     mov esp, ebp
 
 
-    ;; parse kernel elf header
-    ;; parse kernel program header
-    ;; load memory based on program header.
-    call parse_kernel_elf
-    
+    ;; load kernel elf header
+    ;; load memory based on program header.    
     ;; need program offset
     ;; need program segment size
-    mov bx, [kernel_elf.e_phentsize]
-    mov ax, [kernel_elf.e_phnum]
+    mov bx, [elf_offset+e_phentsize_offset]
+    mov cx, [elf_offset+e_phnum_offset]
 
-    ;; step 1 parse program header
-    ;; step 2 move segment to memory
-    ;; go back to step 1
-    mov ecx, 1 			;set up counter for parsing program headers
+    mov eax, program_offset
+
 .LoadKernelSegments:
-    call parse_program_header
+    ;; eax contains the address of the wanted program header
     call load_kernel_segment	
-    add ecx, 1
-    sub ax, 1
-    cmp ax, 0
+    add eax, ebx
+    sub cx, 1
+    cmp cx, 0
     ja .LoadKernelSegments
   
     ;; copy kernel to higher address
@@ -52,34 +47,58 @@ init_pm:
     
     call SwitchToLongMode
 
+    ;; assumes that eax contains address of the program header
 load_kernel_segment:
     push eax
     push ebx
     push ecx
     push edx
-    
-    mov eax, 0x10000
+
+    mov ebx, 0x10000
 
     ;;  this assumes that each page is 4k
     lea edx, [0x100000 - 0x1000]
 
-    add eax, [program_header.p_offset]
-    add edx, [program_header.p_offset]
+    add ebx, [eax + p_offset_offset]
+    add edx, [eax + p_offset_offset]
     mov ecx, 0
 
-    cmp ecx, [program_header.p_memsz]
+    cmp ecx, [eax + p_memsz_offset]
     je .load_kernel_segment_done
-        
-.MoveKernelMemory:
-    mov ebx, [eax]
-    mov [edx], ebx
-    add eax, 4
-    add edx, 4
-    add ecx, 4
-    cmp ecx, [program_header.p_memsz]	
-    jb .MoveKernelMemory
 
     ;; if p_filesz < memsz zero out the rest of the memory
+
+    push ebx
+    push ecx
+    push edx
+
+    mov ebx, 0
+    
+.ZeroOutRegion:
+    mov [edx], ebx
+    add ecx, 4
+    add edx, 4
+    cmp ecx, [eax + p_memsz_offset]
+    jb .ZeroOutRegion
+
+    pop edx
+    pop ecx
+    pop ebx
+    
+.MoveKernelMemory:
+    push eax
+
+    mov eax, [ebx]
+    mov [edx], eax
+    add ebx, 4
+    add edx, 4
+    add ecx, 4
+
+    pop eax
+    
+    cmp ecx, [eax + p_filesz_offset]	
+    jb .MoveKernelMemory
+
 
 .load_kernel_segment_done:    
     
@@ -89,101 +108,31 @@ load_kernel_segment:
     pop eax
     
     ret
-    
-parse_kernel_elf:
-    push eax
-    push ebx
-    push ecx
-    push edx
-    
-    mov ecx, 0
-    mov eax, 0x10000
+        
+elf_offset equ 0x10000
+program_offset equ 0x10040
+    ;; elf header offsets
+e_indent_offset equ 0
+e_type_offset equ 16
+e_machine_offset equ 18
+e_version_offset equ 20
+e_entry_offset equ 24
+e_phoff_offset equ 32
+e_shoff_offset equ 40
+e_flags_offset equ 48
+e_ehsize_offset equ 52
+e_phentsize_offset equ 54
+e_phnum_offset equ 56
+e_shentsize_offset equ 58
+e_shnum_offset equ 60
+e_shstrndx_offset equ 62
 
-fill_elf:
-    mov edx, [eax]
-    mov [kernel_elf + ecx*4], edx
-    add eax, 4
-    add ecx, 1
-    cmp ecx, 16
-    jb fill_elf
-
-    pop edx
-    pop ecx
-    pop ebx
-    pop eax
-    
-    ret
-
-    ;; ebx will hold value for program header length in bytes
-    ;; ecx will hold value for program header number
-parse_program_header:
-    push eax
-    push ebx
-    push ecx
-    push edx
-    
-    mov edx, 0
-    mov eax, 0
-get_header_offset:	
-    add eax, edx
-    add edx, ebx
-    sub ecx, 1
-    cmp ecx, 0
-    ja get_header_offset
-    
-    add eax, 0x40
-
-    push ebx
-    mov ebx, eax	
-
-    ;; ebx now contains offset the beginning of the wanted program header
-    mov eax, 0x10000
-    add eax, ebx
-
-    ;; ebx now contains length of program header in bytes
-    pop ebx
-
-    ;; eax contains the address of the wanted program header
-    mov edx, 0
-fill_header:
-    mov cx, [eax]
-    mov [program_header+edx], cx
-    add eax, 1
-    add edx, 1
-    cmp edx, ebx
-    jne fill_header
-
-    pop edx
-    pop ecx
-    pop ebx
-    pop eax
-
-    ret
-    
-program_header:	
-    .p_type:	dd 0
-    .p_flags:	dd 0
-    .p_offset:	db 0,0,0,0,0,0,0,0
-    .p_vaddr:	db 0,0,0,0,0,0,0,0
-    .p_paddr:	db 0,0,0,0,0,0,0,0
-    .p_filesz:	db 0,0,0,0,0,0,0,0
-    .p_memsz:	db 0,0,0,0,0,0,0,0
-    .p_align:	db 0,0,0,0,0,0,0,0
-
-    
-kernel_elf:
-    .e_indent: db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0  
-    .e_type: dw 0
-    .e_machine: dw 0
-    .e_version: dd 0
-    .e_entry: db 0,0,0,0,0,0,0,0
-    .e_phoff: db 0,0,0,0,0,0,0,0
-    .e_shoff: db 0,0,0,0,0,0,0,0
-    .e_flags: dd 0
-    .e_ehsize: dw 0
-    .e_phentsize: dw 0
-    .e_phnum: dw 0
-    .e_shentsize: dw 0
-    .e_shnum: dw 0
-    .e_shstrndx: dw 0
-
+    ;; program header offsets
+p_type_offset equ 0	
+p_flags_offset equ 4	
+p_offset_offset equ 8	
+p_vaddr_offset equ 16	
+p_paddr_offset equ 24	
+p_filesz_offset equ 32	
+p_memsz_offset equ 40
+p_align_offset equ 48
